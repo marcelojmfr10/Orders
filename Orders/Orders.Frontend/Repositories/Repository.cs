f51245dcc632b8b001869674338c300f04accf2e
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-11-07 18:46:27 -0600

    Add methods to the repository.

--- Orders/Orders.Frontend/Repositories/Repository.cs
+++ Orders/Orders.Frontend/Repositories/Repository.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Reflection;
 using System.Text;
 using System.Text.Json;
 
@@ -6,23 +7,29 @@ namespace Orders.Frontend.Repositories
 {
     public class Repository : IRepository
     {
+        // atributos privados
         private readonly HttpClient _httpClient;
         private JsonSerializerOptions _jsonDefaultOptions => new JsonSerializerOptions
         {
             PropertyNameCaseInsensitive = true
         };
 
+        // constructor
         public Repository(HttpClient httpClient)
         {
             _httpClient = httpClient;
         }
+
+        // propiedades públicas
+        // métodos públicos
+        // métodos privados
 
         public async Task<HttpResponseWrapper<T>> GetAsync<T>(string url)
         {
             var responseHttp = await _httpClient.GetAsync(url);
             if (responseHttp.IsSuccessStatusCode)
             {
-                var response = await UnserializeAnswer<T>(responseHttp);
+                var response = await UnserializeAnswerAsync<T>(responseHttp);
                 return new HttpResponseWrapper<T>(response, false, responseHttp);
             }
 
@@ -44,14 +51,41 @@ namespace Orders.Frontend.Repositories
             var responseHttp = await _httpClient.PostAsync(url, messageContent);
             if (responseHttp.IsSuccessStatusCode)
             {
-                var response = await UnserializeAnswer<TActionResponse>(responseHttp);
+                var response = await UnserializeAnswerAsync<TActionResponse>(responseHttp);
                 return new HttpResponseWrapper<TActionResponse>(response, false, responseHttp);
             }
 
             return new HttpResponseWrapper<TActionResponse>(default, true, responseHttp);
         }
 
-        private async Task<T> UnserializeAnswer<T>(HttpResponseMessage responseHttp)
+        public async Task<HttpResponseWrapper<object>> DeleteAsync<T>(string url)
+        {
+            var responseHttp = await _httpClient.DeleteAsync(url);
+            return new HttpResponseWrapper<object>(null, !responseHttp.IsSuccessStatusCode, responseHttp);
+        }
+
+        public async Task<HttpResponseWrapper<object>> PutAsync<T>(string url, T model)
+        {
+            var messageJson = JsonSerializer.Serialize(model);
+            var messageContent = new StringContent(messageJson, Encoding.UTF8, "application/json");
+            var responseHttp = await _httpClient.PutAsync(url, messageContent);
+            return new HttpResponseWrapper<object>(null, !responseHttp.IsSuccessStatusCode, responseHttp);
+        }
+
+        public async Task<HttpResponseWrapper<TActionResponse>> PutAsync<T, TActionResponse>(string url, T model)
+        {
+            var messageJson = JsonSerializer.Serialize(model);
+            var messageContent = new StringContent(messageJson, Encoding.UTF8, "application/json");
+            var responseHttp = await _httpClient.PutAsync(url, messageContent);
+            if (responseHttp.IsSuccessStatusCode)
+            {
+                var response = await UnserializeAnswerAsync<TActionResponse>(responseHttp);
+                return new HttpResponseWrapper<TActionResponse>(response, false, responseHttp);
+            }
+
+            return new HttpResponseWrapper<TActionResponse>(default, true, responseHttp);
+        }
+        private async Task<T> UnserializeAnswerAsync<T>(HttpResponseMessage responseHttp)
         {
             var response = await responseHttp.Content.ReadAsStringAsync();
             return JsonSerializer.Deserialize<T>(response, _jsonDefaultOptions)!;
