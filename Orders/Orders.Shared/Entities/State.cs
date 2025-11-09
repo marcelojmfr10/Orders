0bdd2d5b3152e5ca19478100a8c09991b68f87df
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-11-09 14:07:04 -0600

    Add State and City Entities.

--- Orders/Orders.Shared/Entities/State.cs
+++ Orders/Orders.Shared/Entities/State.cs
@@ -8,17 +8,20 @@ using System.Threading.Tasks;
 
 namespace Orders.Shared.Entities
 {
-    public class Country : IEntityWithName
+    public class State : IEntityWithName
     {
         public int Id { get; set; }
 
-        [Display(Name = "País")]
+        [Display(Name = "Departamento / Estado")]
         [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
         [Required(ErrorMessage = "El campo {0} es requerido.")]
         public string Name { get; set; } = null!;
 
-        public ICollection<State>? States { get; set; }
-        [Display(Name = "Departamentos / Estados")]
-        public int StatesNumber => States == null || States.Count == 0 ? 0 : States.Count;
+        public int CountryId { get; set; }
+        public Country? Country { get; set; }
+
+        public ICollection<City>? Cities { get; set; }
+        [Display(Name = "Ciudades")]
+        public int CitiesNumber => Cities == null || Cities.Count == 0 ? 0 : Cities.Count;
     }
 }
