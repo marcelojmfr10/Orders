478262eb948ab0596122045ace1aef8e297f08ff
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-11-09 15:15:06 -0600

    Add state specific repository and a state and city controller.

---
+++ Orders/Orders.Backend/Controllers/StatesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orders.Backend.UnitsOfWork.Implementations;
+using Orders.Backend.UnitsOfWork.Interfaces;
+using Orders.Shared.Entities;
+
+namespace Orders.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StatesController : GenericController<State>
+    {
+        private readonly IStatesUnitOfWork _statesUnitOfWork;
+
+        public StatesController(IGenericUnitOfWork<State> unitOfWork, IStatesUnitOfWork statesUnitOfWork) : base(unitOfWork)
+        {
+            _statesUnitOfWork = statesUnitOfWork;
+        }
+
+        [HttpGet]
+        public override async Task<ActionResult> GetAsync()
+        {
+            var action = await _statesUnitOfWork.GetAsync();
+            if (action.WasSuccess)
+            {
+                return Ok(action.Result);
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("{id}")]
+        public override async Task<ActionResult> GetAsync(int id)
+        {
+            var action = await _statesUnitOfWork.GetAsync(id);
+            if (action.WasSuccess)
+            {
+                return Ok(action.Result);
+            }
+            return NotFound(action.Message);
+        }
+    }
+}
--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -25,6 +25,9 @@ builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepositor
 builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
 builder.Services.AddScoped<ICountriesUnitOfWork, CountriesUnitOfWork>();
 
+builder.Services.AddScoped<IStatesRepository, StatesRepository>();
+builder.Services.AddScoped<IStatesUnitOfWork, StatesUnitOfWork>();
+
 var app = builder.Build();
 
 SeedData(app);
