944638045e41e8943bb4e6589204e02bcf2dcdb4
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-11-15 11:26:09 -0600

    Login, logout and register.

---
+++ Orders/Orders.Frontend/AuthenticationProviders/AuthenticationProviderJWT.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
+using Orders.Frontend.Helpers;
+using Orders.Frontend.Services;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+
+namespace Orders.Frontend.AuthenticationProviders
+{
+    public class AuthenticationProviderJWT : AuthenticationStateProvider, ILoginService
+    {
+        private readonly IJSRuntime _jsRunTime;
+        private readonly HttpClient _httpClient;
+        private readonly string _tokenKey;
+        private readonly AuthenticationState _anonimous;
+
+        public AuthenticationProviderJWT(IJSRuntime jsRunTime, HttpClient httpClient)
+        {
+            _jsRunTime = jsRunTime;
+            _httpClient = httpClient;
+            _tokenKey = "TOKEN_KEY";
+            _anonimous = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var token = await _jsRunTime.GetLocalStorage(_tokenKey);
+            if(token is null)
+            {
+                return _anonimous;
+            }
+
+            return BuildAuthenticationState(token.ToString()!);
+        }
+
+        public async Task LoginAsync(string token)
+        {
+            await _jsRunTime.SetLocalStorage(_tokenKey, token);
+            var authState = BuildAuthenticationState(token);
+            NotifyAuthenticationStateChanged(Task.FromResult(authState));
+        }
+
+        public async Task LogoutAsync()
+        {
+            await _jsRunTime.RemoveLocalStorage(_tokenKey);
+            _httpClient.DefaultRequestHeaders.Authorization = null;
+            NotifyAuthenticationStateChanged(Task.FromResult(_anonimous));
+        }
+
+        private AuthenticationState BuildAuthenticationState(string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
+            var claims = ParseClaimsFromJWT(token);
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(claims, "jwt")));
+        }
+
+        private IEnumerable<Claim> ParseClaimsFromJWT(string token)
+        {
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var unserializedToken = jwtSecurityTokenHandler.ReadJwtToken(token);
+            return unserializedToken.Claims;
+        }
+    }
+}
