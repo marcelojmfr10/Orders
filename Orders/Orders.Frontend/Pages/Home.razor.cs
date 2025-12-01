8fea1d60cef7f5d9e68ef2ea1ba5c6eaf9eaa0d1
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-11-30 19:18:05 -0600

    Add products to the shopping cart.

--- Orders/Orders.Frontend/Pages/Home.razor.cs
+++ Orders/Orders.Frontend/Pages/Home.razor.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Orders.Frontend.Pages.Auth;
 using Orders.Frontend.Repositories;
+using Orders.Shared.DTOs;
 using Orders.Shared.Entities;
 
 namespace Orders.Frontend.Pages
@@ -12,13 +13,9 @@ namespace Orders.Frontend.Pages
     {
         private int currentPage = 1;
         private int totalPages;
-        //private int counter = 0;
-        //private bool isAuthenticated;
-        //private string allCategories = "all_categories_list";
-
+        private int counter = 0;
+        private bool isAuthenticated;
         public List<Product>? Products { get; set; }
-        //public List<Category>? Categories { get; set; }
-        //public string CategoryFilter { get; set; } = string.Empty;
 
 
         [Parameter, SupplyParameterFromQuery] public string Page { get; set; } = string.Empty;
@@ -28,54 +25,40 @@ namespace Orders.Frontend.Pages
         [Inject] private IRepository Repository { get; set; } = null!;
         [Parameter, SupplyParameterFromQuery] public int RecordsNumber { get; set; } = 8;
 
-        //[CascadingParameter] private Task<AuthenticationState> authenticationStateTask { get; set; } = null!;
-        //[CascadingParameter] private IModalService Modal { get; set; } = default!;
+        [CascadingParameter] private Task<AuthenticationState> authenticationStateTask { get; set; } = null!;
+        [CascadingParameter] private IModalService Modal { get; set; } = default!;
 
         protected override async Task OnInitializedAsync()
         {
             await LoadAsync();
         }
 
-        //protected async override Task OnParametersSetAsync()
-        //{
-        //    await CheckIsAuthenticatedAsync();
-        //    await LoadCounterAsync();
-        //    await LoadCategoriesAsync();
-        //}
+        protected async override Task OnParametersSetAsync()
+        {
+            await CheckIsAuthenticatedAsync();
+            await LoadCounterAsync();
+        }
 
-        //private async Task LoadCategoriesAsync()
-        //{
-        //    var responseHttp = await Repository.GetAsync<List<Category>>("api/categories/combo");
-        //    if (responseHttp.Error)
-        //    {
-        //        var message = await responseHttp.GetErrorMessageAsync();
-        //        await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
-        //    }
+        private async Task CheckIsAuthenticatedAsync()
+        {
+            var authenticationState = await authenticationStateTask;
+            isAuthenticated = authenticationState.User.Identity!.IsAuthenticated;
+        }
 
-        //    Categories = responseHttp.Response;
-        //}
+        private async Task LoadCounterAsync()
+        {
+            if (!isAuthenticated)
+            {
+                return;
+            }
 
-
-        //private async Task CheckIsAuthenticatedAsync()
-        //{
-        //    var authenticationState = await authenticationStateTask;
-        //    isAuthenticated = authenticationState.User.Identity!.IsAuthenticated;
-        //}
-
-        //private async Task LoadCounterAsync()
-        //{
-        //    if (!isAuthenticated)
-        //    {
-        //        return;
-        //    }
-
-        //    var responseHttp = await Repository.GetAsync<int>("/api/temporalOrders/count");
-        //    if (responseHttp.Error)
-        //    {
-        //        return;
-        //    }
-        //    counter = responseHttp.Response;
-        //}
+            var responseHttp = await Repository.GetAsync<int>("/api/temporalOrders/count");
+            if (responseHttp.Error)
+            {
+                return;
+            }
+            counter = responseHttp.Response;
+        }
 
         private async Task SelectedRecordsNumberAsync(int recordsnumber)
         {
@@ -100,18 +83,6 @@ namespace Orders.Frontend.Pages
 
         private async Task LoadAsync(int page = 1)
         {
-            //if (!string.IsNullOrWhiteSpace(category))
-            //{
-            //    if (category == allCategories)
-            //    {
-            //        CategoryFilter = string.Empty;
-            //    }
-            //    else
-            //    {
-            //        CategoryFilter = category;
-            //    }
-            //}
-
             if (!string.IsNullOrWhiteSpace(Page))
             {
                 page = Convert.ToInt32(Page);
@@ -141,11 +112,6 @@ namespace Orders.Frontend.Pages
                 url += $"&filter={Filter}";
             }
 
-            //if (!string.IsNullOrEmpty(CategoryFilter))
-            //{
-            //    url += $"&CategoryFilter={CategoryFilter}";
-            //}
-
             var response = await Repository.GetAsync<List<Product>>(url);
             if (response.Error)
             {
@@ -165,10 +131,6 @@ namespace Orders.Frontend.Pages
             {
                 url += $"&filter={Filter}";
             }
-            //if (!string.IsNullOrEmpty(CategoryFilter))
-            //{
-            //    url += $"&CategoryFilter={CategoryFilter}";
-            //}
 
             var response = await Repository.GetAsync<int>(url);
             if (response.Error)
@@ -189,43 +151,43 @@ namespace Orders.Frontend.Pages
 
         private async Task AddToCartAsync(int productId)
         {
-            //if (!isAuthenticated)
-            //{
-            //    Modal.Show<Login>();
-            //    var toast1 = SweetAlertService.Mixin(new SweetAlertOptions
-            //    {
-            //        Toast = true,
-            //        Position = SweetAlertPosition.BottomEnd,
-            //        ShowConfirmButton = false,
-            //        Timer = 3000
-            //    });
-            //    await toast1.FireAsync(icon: SweetAlertIcon.Error, message: "Debes haber iniciado sesión para poder agregar productos al carro de compras.");
-            //    return;
-            //}
+            if (!isAuthenticated)
+            {
+                Modal.Show<Login>();
+                var toast1 = SweetAlertService.Mixin(new SweetAlertOptions
+                {
+                    Toast = true,
+                    Position = SweetAlertPosition.BottomEnd,
+                    ShowConfirmButton = false,
+                    Timer = 3000
+                });
+                await toast1.FireAsync(icon: SweetAlertIcon.Error, message: "Debes haber iniciado sesión para poder agregar productos al carro de compras.");
+                return;
+            }
 
-            //var temporalOrderDTO = new TemporalOrderDTO
-            //{
-            //    ProductId = productId
-            //};
+            var temporalOrderDTO = new TemporalOrderDTO
+            {
+                ProductId = productId
+            };
 
-            //var httpActionResponse = await Repository.PostAsync("/api/temporalOrders/full", temporalOrderDTO);
-            //if (httpActionResponse.Error)
-            //{
-            //    var message = await httpActionResponse.GetErrorMessageAsync();
-            //    await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
-            //    return;
-            //}
+            var httpActionResponse = await Repository.PostAsync("/api/temporalOrders/full", temporalOrderDTO);
+            if (httpActionResponse.Error)
+            {
+                var message = await httpActionResponse.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
 
-            //await LoadCounterAsync();
+            await LoadCounterAsync();
 
-            //var toast2 = SweetAlertService.Mixin(new SweetAlertOptions
-            //{
-            //    Toast = true,
-            //    Position = SweetAlertPosition.BottomEnd,
-            //    ShowConfirmButton = true,
-            //    Timer = 3000
-            //});
-            //await toast2.FireAsync(icon: SweetAlertIcon.Success, message: "Producto agregado al carro de compras.");
+            var toast2 = SweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = true,
+                Timer = 3000
+            });
+            await toast2.FireAsync(icon: SweetAlertIcon.Success, message: "Producto agregado al carro de compras.");
         }
     }
 }
